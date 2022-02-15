297b15c9c489ed8847d3cabc55e7d12570760648
Joseph <jyonalee@github.com>
2022-02-15 15:21:52 -0800

    BRMB-42: adding second cl node with same settings

--- main.tf
+++ main.tf
@@ -24,6 +24,10 @@ variable "cl1_name" {
   default     = "aeolus"
   description = "name of the first node"
 }
+variable "cl2_name" {
+  default     = "boreas"
+  description = "name of the first node"
+}
 variable "chainlink_version" {
   default     = "1.1.0"
   description = "chainlink node client version"
@@ -35,6 +39,27 @@ provider "digitalocean" {}
 resource "digitalocean_droplet" "chainlink_node1" {
   image      = "ubuntu-20-04-x64"
   name       = "chainlink-${var.network}-${var.cl1_name}"
+  region     = var.region1
+  size       = var.instance_size_1
+  tags       = ["chainlink"]
+  monitoring = true
+  vpc_uuid   = var.vpc1_uuid
+  user_data  = templatefile("cl_setup.yaml", {
+    ssh_public_key     = var.ssh_public_key,
+    gateway_private_ip = var.ig1_private_ip,
+    network            = var.network,
+    cl_client_version  = var.chainlink_version,
+    api_user           = var.api_user,
+    api_pw             = var.api_pw,
+    wallet_pw          = var.wallet_pw,
+    backup_eth_url     = var.backup_eth_url,
+    db_conn_str        = var.db_conn_str
+  })
+}
+
+resource "digitalocean_droplet" "chainlink_node2" {
+  image      = "ubuntu-20-04-x64"
+  name       = "chainlink-${var.network}-${var.cl2_name}"
   region     = var.region1
   size       = var.instance_size_1
   tags       = ["chainlink"]
